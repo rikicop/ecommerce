eaa9f4e272de7f1ee765608745a714d5d96fdc5d
rikicop <ruperto1@protonmail.com>
2021-01-13 12:32:22 -0400

    ecommerce migration 3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -68,10 +68,22 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'laudable_db',
+        'USER':'laudable_user',
+        'PASSWORD':'laudable_password',
+        'HOST' : 'localhost',
+        'PORT' : '5432', 
     }
 }
 
